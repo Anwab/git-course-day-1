30bff785904532d87e503f29edfbe9e83824a4f7
Wallsby Anna (BT) <anna.wallsby@hm.com>
2021-04-09 12:00:06 +0200

    OneYearPasses

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String Name= "Anna";
+        int age = 15;
+
+        oneYearPasses (age);
+
+         }
+        public static int oneYearPasses (int age){
+            System.out.println( "Anna turned " + (age+1)+ " years old.");
+            return age+1;
+
+	// write your code here
+        }
+        }
